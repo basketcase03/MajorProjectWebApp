0472649e53b221d7d2bf48f71d880d4ed1d7ead8
basketcase03 <muskankhajuria03@gmail.com>
2021-12-10 00:32:48 +0530

    fixed dataframe

--- website/depression_detection.py
+++ website/depression_detection.py
@@ -9,7 +9,7 @@ def get_status(feats):
 
     feat_cols = ['PFQ090', 'INQ060', 'DBQ197', 'CBD071', 'WHQ225', 'CBQ596', 'MCQ300C', 'MCQ160F', 'INQ150', 'DMDMARTL', 'SDMVSTRA', 'AUQ054', 'CBD091', 
     'HUQ071', 'DLQ150', 'HSAQUEX', 'HUQ010', 'INQ140', 'DLQ020', 'MCQ160L', 'FSD032B', 'MIALANG', 'DLQ140', 'RIDRETH1', 'DBD910']
-    df = pandas.DataFrame([feat_cols])
+    df = pandas.DataFrame([feats])
     df = df.reindex(columns=feat_cols)
     X = df.loc[:, feat_cols].values
     res = loaded_model.predict(X)
